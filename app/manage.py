90c9e36e7a546b6b2be343ce91988b963fd3605e
huangtao00 <397916230@qq.com>
2018-03-12 21:52:17 +0800

    update blockquote

--- app/manage.py
+++ app/manage.py
@@ -36,5 +36,7 @@ def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
+
+print app.url_map._rules
 if __name__ == "__main__":
     manager.run()
